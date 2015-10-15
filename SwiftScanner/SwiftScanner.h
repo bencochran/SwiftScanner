65741605dc8178eed52a8b76048d9a6b1512c5ff
Ben Cochran <bcochran@gmail.com>
2015-10-15 09:46:58 -0700

    Initial Commit

---
+++ SwiftScanner/SwiftScanner.h
@@ -0,0 +1,19 @@
+//
+//  SwiftScanner.h
+//  SwiftScanner
+//
+//  Created by Ben Cochran on 10/15/15.
+//  Copyright © 2015 Ben Cochran. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftScanner.
+FOUNDATION_EXPORT double SwiftScannerVersionNumber;
+
+//! Project version string for SwiftScanner.
+FOUNDATION_EXPORT const unsigned char SwiftScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftScanner/PublicHeader.h>
+
+
